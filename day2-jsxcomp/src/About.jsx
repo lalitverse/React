c0e3133f5bced8288865ce12d1e5ddd08e89014a
Lalit Hirvey <lalithirvey838@gmail.com>
2026-06-24 22:00:04 +0530

    Updated project

---
+++ day2-jsxcomp/src/About.jsx
@@ -0,0 +1,5 @@
+let About = () => {
+    return "Hii";
+}
+
+export default About;
---
+++ day2-jsxcomp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import About from "./About"
+
+let App = () => {
+  return (
+    <nav>
+      <h2>Hello</h2>
+    </nav>
+    
+  )
+  
+}
+<About/>
+
+export default App;
